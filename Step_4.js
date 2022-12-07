bb81b98e82479fc72312b850c7cfe67a1a938d0c
Mohamad Moughnie <mhdmoughnie@gmail.com>
2022-12-07 14:08:15 +0200

    js-basic-01

---
+++ Step_4.js
@@ -0,0 +1,8 @@
+
+var name1 = prompt("enter your name", "name..");
+
+var surname = prompt("enter your surname", "surname..");
+
+var city = prompt("enter your city", "city..");
+
+alert ("Hello! " + name1 + " " + surname + " from " + city);
--- step_4.js
+++
@@ -1,4 +0,0 @@
-var name1 = Prompt("enter your name", "name..")
-var surname = prompt("enter your surname", "surname..")
-var city = prompt("enter your city", "city..")
-alert ("Hello! " + name1 + surname + "from" + city)
