4f1d1a26164047b7f3c9ebd84709d67df51812a0
Mohamad Moughnie <mhdmoughnie@gmail.com>
2022-12-08 22:08:20 +0200

    js-basic03 step1

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 1/step_1.js
@@ -0,0 +1,9 @@
+var imge = document.getElementById('image1')
+/*imge.onmouseover=function(){
+    console.log("12")
+    imge.style.border="3px solid red"
+}*/
+
+imge.addEventListener('mouseover', function(){
+    imge.style.border= "thick solid red";
+})
