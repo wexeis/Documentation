26ea8aee8a1c331d347973d4a66f900cd1b8536c
Mohamad Moughnie <mhdmoughnie@gmail.com>
2022-12-08 20:27:31 +0200

    js-basic-02 step 4

--- 03_Exercises/03_JavaScript/javascript-basics-02/step 4/step-4.js
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 4/step-4.js
@@ -1,6 +1,22 @@
-var btn = document.getElementsByName('button')
-btn.addEventListener('onclick', function(){
-    var name1 = document.getElementById('name')
-    var surname= document.getElementById('surname')
-    var city = document.getElementById('city')
-})
+alert("hello");
+var reset1 = document.getElementById('reset')
+/*reset1.addEventListener('click', function() {
+
+    document.getElementById("name").value = "";
+    document.getElementById("surname").value = "";
+    document.getElementById("city").value = ""}
+)*/
+reset1.onclick=function(){
+    let confirmMsg= confirm("Are you sure?")
+    console.log(confirmMsg)
+    if(confirmMsg == true){
+    
+    document.getElementById("name").value = "";
+    document.getElementById("surname").value = "";
+    document.getElementById("city").value = "";
+    return alert("Info deleted")
+}
+else {
+    return alert("Info are not deleted")
+}
+}   
