f28773e5dddb9a96882ee0372335753f1a370b14
Mohamad Moughnie <mhdmoughnie@gmail.com>
2022-12-09 15:39:01 +0200

    jsbasic02 step 5-6 fix

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 5/step 5.js
@@ -0,0 +1,7 @@
+function myfunction(){
+    document.getElementById("image1").src="./images/image1_2.jpg";
+    document.getElementById("image2").src="./images/image2_2.jpg";
+    document.getElementById("image3").src="./images/image3_2.jpg";
+    document.getElementById("image4").src="./images/image4_2.jpg";
+    document.getElementById("image5").src="./images/image5_2.jpg";
+}
--- 03_Exercises/03_JavaScript/javascript-basics-02/step 6/step-6.js
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 6/step-6.js
@@ -1,41 +1,16 @@
-var a = document.getElementsByTagName('img')
+function f1(){
+    document.getElementById("image1").src="./images/image1_2.jpg";
+    document.getElementById("image2").src="./images/image2_2.jpg";
+    document.getElementById("image3").src="./images/image3_2.jpg";
+    document.getElementById("image4").src="./images/image4_2.jpg";
+    document.getElementById("image5").src="./images/image5_2.jpg";
+    
+}
+function f2(){
+    document.getElementById("image1").src="./images/image1.jpg";
+    document.getElementById("image2").src="./images/image2.jpg";
+    document.getElementById("image3").src="./images/image3.jpg";
+    document.getElementById("image4").src="./images/image4.jpg";
+    document.getElementById("image5").src="./images/image5.jpg";
 
-a.addEventListener('mouseover', function(){
-    console.log("mousever")
-    if(a.id="imager1"){
-return a.src = "./images/image1_2.jpg";
 }
-
-else if(a.id='image2'){
-    return a.src = "./images/image2_2.jpg";
-}
-else if(a.id='image3'){
-    return a.src = "./images/image3_2.jpg";
-}
-else if(a.id='image4'){
-    return a.src = "./images/image4_2.jpg";
-}
-else if(a.id='image5'){
-    return a.src = "./images/image5_2.jpg";
-}
-})
-
-.addEventListener('mouseover', function(){
-    console.log("mousever")
-    if(a.id="image1"){
-return a.src = "./images/image1.jpg";
-}
-
-else if(a.id='image2'){
-    return a.src = "./images/image2.jpg";
-}
-else if(a.id='image3'){
-    return a.src = "./images/image3.jpg";
-}
-else if(a.id='image4'){
-    return a.src = "./images/image4.jpg";
-}
-else if(a.id='image5'){
-    return a.src = "./images/image5.jpg";
-}
-})
